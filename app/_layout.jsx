c542e480e7b06ebfbf95a861d30325f98817c2ba
kishoear <kishorear.programming@gmail.com>
2024-06-12 17:57:47 +0530

    Asset collection

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text } from 'react-native'
+import { Stack } from 'expo-router'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const RootLayout = () =>
+{
+    return(
+        <Stack>
+            <Stack.Screen name='index' options={{headerShown : false}} />
+            <Stack.Screen name='(auth)' options={{headerShown : false}} />
+            <Stack.Screen name='(tabs)' options={{headerShown : false}} />
+        </Stack>
+    )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default RootLayout
